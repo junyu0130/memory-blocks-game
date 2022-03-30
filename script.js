134ba2457416495b41d81903547fc346ecc1afed
JunYu <tony.rex.tony.rex01305104@gmail.com>
2022-03-30 23:11:05 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const iconSet = [
+  "at",
+  "apple",
+  "archive",
+  "box",
+  "bug",
+  "badge-vr",
+  "bag-plus",
+  "bootstrap",
+  "bluetooth",
+  "battery-full",
+  "bookmark-check",
+  "cpu",
+  "cast",
+  "camera",
+  "code-slash",
+  "cloud-slash",
+  "camera-video",
+  "calendar-heart",
+  "clipboard-pulse",
+  "chat-square-quote",
+];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffle(iconSet);
+
+class Blocks {
+  constructor(iconSet, mode) {
+    this.imgData = iconSet.slice(0, 8).concat(iconSet.slice(0, 8));
+    this.mode = mode;
+  }
+
+  setImg() {
+    shuffle(this.imgData);
+    for (let i = 0; i < this.imgData.length; i++) {
+      const img = this.imgData[i];
+      $(".blocks." + this.mode + " .block" + i + " i").addClass("bi-" + img);
+    }
+  }
+}
+
+var b = new Blocks(iconSet, "x4");
+b.setImg();
