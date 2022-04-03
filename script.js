8309bb702dcd1cec0ba166be980190263538cfff
JunYu <tony.rex.tony.rex01305104@gmail.com>
2022-04-03 19:42:35 +0800

    basic game

--- script.js
+++ script.js
@@ -32,8 +32,11 @@ shuffle(iconSet);
 
 class Blocks {
   constructor(iconSet, mode) {
-    this.imgData = iconSet.slice(0, 8).concat(iconSet.slice(0, 8));
     this.mode = mode;
+    this.size = Math.pow(Number(this.mode[1]), 2);
+    this.imgData = iconSet
+      .slice(0, this.size / 2)
+      .concat(iconSet.slice(0, this.size / 2));
   }
 
   setImg() {
@@ -44,12 +47,94 @@ class Blocks {
   }
 
   flipImg(index) {
-    $(".blocks." + this.mode + " .block" + index + " i").fadeToggle(
-      100,
-      () => {}
-    );
+    $(".blocks." + this.mode + " .block" + index + " i").fadeToggle(100);
   }
 }
 
-var b = new Blocks(iconSet, "x4");
-b.setImg();
+class Game {
+  constructor() {
+    this.blocks = new Blocks(iconSet, "x4");
+    this.setTiming(0, 30);
+    this.ansSet = [];
+    this.status = "waiting";
+  }
+
+  setTiming(min, sec) {
+    this.timing = (min * 60 + sec) * 1000;
+  }
+
+  setAnsSet() {
+    iconSet.slice(0, this.blocks.size / 2).forEach((icon) => {
+      this.ansSet.push([
+        this.blocks.imgData.indexOf(icon),
+        this.blocks.imgData.lastIndexOf(icon),
+      ]);
+    });
+  }
+
+  hideAllImg() {
+    for (let i = 0; i < this.blocks.size; i++) {
+      if (
+        $(".blocks." + this.blocks.mode + " .block" + i + " i").css(
+          "display"
+        ) != "none"
+      ) {
+        this.blocks.flipImg(i);
+      }
+    }
+  }
+
+  startUserInput() {
+    this.userInput = [];
+    this.status = "userInput";
+  }
+
+  sendInput(inputNum) {
+    if (this.status == "userInput") {
+      if (
+        $(".blocks." + this.blocks.mode + " .block" + inputNum + " i").css(
+          "display"
+        ) == "none"
+      ) {
+        this.blocks.flipImg(inputNum);
+        this.userInput.push(inputNum);
+        this.userInput.sort((a, b) => a - b);
+        console.log(this.userInput);
+      }
+
+      if (this.userInput.length == 2) {
+        if (
+          this.ansSet.findIndex(
+            (ans) => ans[0] == this.userInput[0] && ans[1] == this.userInput[1]
+          ) != -1
+        ) {
+          console.log("correct!");
+          // correct sound
+        } else {
+          console.log("wrong");
+        }
+        this.userInput = [];
+      }
+    }
+  }
+
+  startGame() {
+    // set image
+    this.blocks.setImg();
+
+    // setup answer set
+    this.setAnsSet();
+
+    // // start timer
+    // setTimeout(() => {
+    //   // hide all image
+    //   this.hideAllImg();
+    // }, this.timing);
+
+    // start and check answer
+    this.startUserInput();
+  }
+}
+
+var g = new Game();
+g.startGame();
